cfe51fa952a5ec78af99e55db2594dc92a6bd83e
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-04-02 00:15:47 +0530

    index modified

--- src/index.js
+++ src/index.js
@@ -1,31 +1,46 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-import VideoList from './components/video_list'
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyDjxEPlALmeVymiliH22_z_4OuDAy7rLwU';
-
-
 
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {videos : []};
+    this.state = {
+
+      videos: [],
+      selectedVideo: null
+
+    };
+
+    this.videoSearch('ICC');
+  }
+  videoSearch(term){
     YTSearch(
-      { key: API_KEY, term: 'Cat' }, (videos)=> 
+      { key: API_KEY, term: term }, (videos)=> 
         {
-          this.setState({videos});
-          console.log(videos); 
+          this.setState({
+            videos: videos,
+            selectedVideo: videos[0]
+          });
         }
     );
-  }
+  };
+  
   
   render(){
     return (
       <div>
-        <SearchBar />
-        <VideoList videos = {this.state.videos} />
+        <SearchBar onSearchTermChange={term=>this.videoSearch(term)}/>
+        <VideoDetail video ={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+          videos = {this.state.videos} 
+        />
       </div>
     );
 
